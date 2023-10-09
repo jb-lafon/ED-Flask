56103b0f1ca711d8641236e9f81dbe76113ed6d0
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2023-10-08 18:03:50 -0700

    EMPTY PROJECT

--- app/alchemydb/__init__.py
+++
@@ -1,7 +0,0 @@
-# ==========
-# File : __init__.py.py
-# Author : Jb
-# First created on : 23/11/2018
-# Description:
-# ==========
-
--- instance/config.py
+++
@@ -1,9 +0,0 @@
-# ==========
-# File : config.py
-# Author : Jb
-# First created on : 21/11/2018
-# Description:
-# ==========
-
-SECRET_KEY = 'swjkhawhvfuygh'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:pwBQTd1942dR@localhost/developmentdb'
