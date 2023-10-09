101af2e4b93b9bc2133ffbbc41d340289f0902ed
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2023-10-09 02:26:11 -0700

    Comments

--- starmaptools/starmaptools/classes.py
+++ starmaptools/starmaptools/classes.py
@@ -3,6 +3,15 @@ from .config import *
 import pandas as pd
 from sqlalchemy import create_engine
 
+#---------------------------------------------------------------------------
+# *                           ENGINE
+# ?  Main class for connecting to database
+# @param hostname string  
+# @param dbname string  
+# @param uname string  
+# @param pwd string  
+#---------------------------------------------------------------------------
+
 class Engine:
     # DEFALUT Credentials to database connection import from _lib.config
     HOSTNAME=HOSTNAME
@@ -27,24 +36,46 @@ class Engine:
         for (key, value) in databases.items():
             self.__setattr__(key, value)
         return self
+#----------------------------- END OF SECTION ------------------------------
+
+    
+#---------------------------------------------------------------------------
+# *                           ENGINE
+# ?  Main class for connecting to database
+# @param hostname string  
+# @param dbname string  
+# @param uname string  
+# @param pwd string  
+#---------------------------------------------------------------------------
     
 class SQLQuery:
-    def __new__(self, query="", filter="", limit=None, ENGINE=Engine().ENGINE, TABLE='systemswithcoordinates'):
+    def __new__(self, query="", filter="", limit=None, ENGINE=Engine().ENGINE, table='systemswithcoordinates'):
         self.ENGINE = ENGINE
-        self.TABLE = TABLE
-        query  = query + "FROM {d}.{t}".format(d=DBNAME, t=self.TABLE)
+        query  = query + "FROM {d}.{t}".format(d=DBNAME, t=table)
         if filter != '':
             query = query + " WHERE {f}".format(f=filter)
         if limit != None:
             query = query + " LIMIT {l}".format(l=limit)
         response = pd.read_sql_query(query, self.ENGINE)
         return response
+#----------------------------- END OF SECTION ------------------------------
+
+
+#---------------------------------------------------------------------------
+# *                           SYSTEMS
+# ?  Main class for connecting to database
+#---------------------------------------------------------------------------
          
 class Systems:
     def __init__(self, ENGINE=Engine().ENGINE, TABLE='systemswithcoordinates'):
         self.ENGINE = ENGINE
         self.TABLE = TABLE
         return None
+    
+    #===========================
+    # *   all
+    # ? Returns systems with corresponding name
+    #===========================
     
     def all(self, filter='', limit=None):
         q = "SELECT * FROM {db}.{t}".format(db=DBNAME, t=self.TABLE)
@@ -54,6 +85,13 @@ class Systems:
             q = q + " LIMIT {l}".format(l=limit)
         systems = pd.read_sql_query(q, self.ENGINE)
         return systems
+    
+    #===========================
+    # *   find_by_name
+    # ? Returns systems with corresponding name
+    # @param system_name string  
+    # @return pd.Dataframe
+    #===========================
     
     def find_by_name(self, system_name=""):
         q = Engine.select_all(self.TABLE) + "WHERE name='{n}'".format(n=system_name)
--- starmaptools/starmaptools/functions.py
+++ starmaptools/starmaptools/functions.py
@@ -1,7 +1,15 @@
 import re
 
+#===========================
+# *   urlify
+#===========================
+
 def urlify(string):
     return string.replace(' ', '+')
+
+#===========================
+# *   find_whole_word
+#===========================
 
 def find_whole_word(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
